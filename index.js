6439743a553733d0ca2beb1eda77041d774195ec
David Welles <74557848+davidwelles@users.noreply.github.com>
2021-03-13 21:34:37 -0500

    js updat

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+//function for displaying image in HTML document
 function myDisplayer (some) {
   document.getElementById('demo').innerHTML = some
 }
-
+//API request for picture
 function getFile (myCallback) {
   const req = new XMLHttpRequest()
   req.open('GET', 'https://images.dog.ceo/breeds/doberman/n02107142_6859.jpg')
   req.onload = function () {
     if (req.status == 200) {
-      myCallback(this.responseText)
+      myCallback(this.responseText)  //Trying to display as an image
     } else {
       myCallback('Error: ' + req.status)
     }
